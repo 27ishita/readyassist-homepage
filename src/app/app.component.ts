0ba408d832432d5da6ff3cae69b81bacd660ceb6
27ishita <ishitadhar_d@srmuniv.edu.in>
2022-01-15 09:36:09 +0530

    reviews added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'taxi-services';
+  gallery = [
+    'https://www.readyassist.in/assets/banner/driveu.png',
+    'https://www.readyassist.in/assets/banner/shell.png',
+  ];
 }
